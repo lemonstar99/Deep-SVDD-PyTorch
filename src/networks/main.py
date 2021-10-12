e85ae1b6953c02bf16dcd8dff6377f0ad3c3e104
lemonstar99 <44111674+lemonstar99@users.noreply.github.com>
2021-10-12 17:22:13 +0900

    identifying TODO for CT dataset

--- src/networks/main.py
+++ src/networks/main.py
@@ -6,7 +6,7 @@ from .cifar10_LeNet_elu import CIFAR10_LeNet_ELU, CIFAR10_LeNet_ELU_Autoencoder
 def build_network(net_name):
     """Builds the neural network."""
 
-    implemented_networks = ('mnist_LeNet', 'cifar10_LeNet', 'cifar10_LeNet_ELU')
+    implemented_networks = ('mnist_LeNet', 'cifar10_LeNet', 'cifar10_LeNet_ELU', 'ct_LeNet')
     assert net_name in implemented_networks
 
     net = None
@@ -20,13 +20,16 @@ def build_network(net_name):
     if net_name == 'cifar10_LeNet_ELU':
         net = CIFAR10_LeNet_ELU()
 
+    if net_name == 'ct_LeNet':
+        net = CT_LeNet()
+
     return net
 
 
 def build_autoencoder(net_name):
     """Builds the corresponding autoencoder network."""
 
-    implemented_networks = ('mnist_LeNet', 'cifar10_LeNet', 'cifar10_LeNet_ELU')
+    implemented_networks = ('mnist_LeNet', 'cifar10_LeNet', 'cifar10_LeNet_ELU', 'ct_LeNet')
     assert net_name in implemented_networks
 
     ae_net = None
@@ -39,5 +42,8 @@ def build_autoencoder(net_name):
 
     if net_name == 'cifar10_LeNet_ELU':
         ae_net = CIFAR10_LeNet_ELU_Autoencoder()
+
+    if net_name == 'ct_LeNet':
+        ae_net = CT_LeNet_Autoencoder()
 
     return ae_net
