6771ae6793732aab6d63c20daec0ac6cb749b91a
lemonstar99 <44111674+lemonstar99@users.noreply.github.com>
2021-10-21 17:21:08 +0900

    Update ct.py

--- src/datasets/ct.py
+++ src/datasets/ct.py
@@ -61,36 +61,6 @@ class CT_Dataset(TorchvisionDataset):
     def __getitem__(self, idx):
         return self.X_train[idx], self.y_train[idx]
     
-    def get_input_data():
-        x = []
-        with open('data/input.csv') as f:
-            single_sequence = []
-            for point in f:
-                if zero_point_that_can_be_skipped in point:
-                    continue
-
-                if single_sequence_end in point:
-                    for i in range(longest_sequence_length_with_trimmed_zeros - len(single_sequence)):
-                        single_sequence.insert(0, padding_vector)
-
-                    x.append(copy.deepcopy(single_sequence))
-
-                    single_sequence = []
-                    continue
-
-                single_sequence.append([])
-                for point_element in point.split(','):
-                    single_sequence[-1].append(float(point_element))
-        return x
-
-
-    def get_output_data():
-        y = []
-        with open('data/output.txt') as f:
-            for character_class in f.readlines()[0].split('|'):
-                y.append(int(character_class) - 1)
-
-        return np_utils.to_categorical(y, number_of_character_classes)
 
 class MyCT(CT_Dataset):
     # Torchvision CIFAR10 class with patch of __getitem__ method to also return the index of a data sample.
@@ -123,6 +93,37 @@ class MyCT(CT_Dataset):
             target = self.target_transform(target)
 
         return img, target, index  # only line changed
+
+    def get_input_data():
+        x = []
+        with open('data/input.csv') as f:
+            single_sequence = []
+            for point in f:
+                if zero_point_that_can_be_skipped in point:
+                    continue
+
+                if single_sequence_end in point:
+                    for i in range(longest_sequence_length_with_trimmed_zeros - len(single_sequence)):
+                        single_sequence.insert(0, padding_vector)
+
+                    x.append(copy.deepcopy(single_sequence))
+
+                    single_sequence = []
+                    continue
+
+                single_sequence.append([])
+                for point_element in point.split(','):
+                    single_sequence[-1].append(float(point_element))
+        return x
+
+
+    def get_output_data():
+        y = []
+        with open('data/output.txt') as f:
+            for character_class in f.readlines()[0].split('|'):
+                y.append(int(character_class) - 1)
+
+        return np_utils.to_categorical(y, number_of_character_classes)
 
 
 
