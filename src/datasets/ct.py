62925a3e1f13acd84c2f348a7051dab2e571c396
lemonstar99 <dabinaseo@gmail.com>
2021-10-22 18:07:02 +0900

    Update ct.py

--- src/datasets/ct.py
+++ src/datasets/ct.py
@@ -48,26 +48,26 @@ class CT_Dataset(TorchvisionDataset):
         x_y = list(zip(x, y))
         random.shuffle(x_y)
         x, y = zip(*x_y)
+
+        # train_set = MyCT(x_values=x, y_values=y, idx=test_count, train=True)
+        # test_set = MyCT(x_values=x, y_values=y, idx=test_count, train=False)
+
+        train_set = Dataset(np.array(x[test_count:]), np.array(y[test_count:]))
+        test_set = Dataset(np.array(x[:test_count]), np.array(y[:test_count])        
         
+        print("checkpoint")
+        print(train_set)
+
         # in this order: x_train, y_train, x_test, y_test
         # np.array(x[test_count:]), np.array(y[test_count:]), np.array(x[:test_count]), np.array(y[:test_count])
 
         # self.X_train = torch.tensor(x_train, dtype=torch.float32)
         # self.y_train = torch.tensor(y_train)
 
-        train_set = MyCT(x_values=x, y_values=y, idx=test_count, train=True)
-        test_set = MyCT(x_values=x, y_values=y, idx=test_count, train=False)
-        
-        print("checkpoint")
-        print(train_set)
-
         # train_idx_normal = get_target_label_idx(train_set.train_labels, self.normal_classes)
         # self.train_set = Subset(train_set, train_idx_normal)
 
         # self.test_set = MyCT(root=self.root)
-
-        # self.train_set = list(zip(np.array(x[test_count:]), np.array(y[test_count:])))
-        # self.test_set = list(zip(np.array(x[:test_count]), np.array(y[:test_count])))
     
 def get_input_data():
     x = []
