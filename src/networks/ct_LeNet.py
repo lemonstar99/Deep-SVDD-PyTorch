0b4b245b4c2e9be8f5cf71bcad880ad51c469117
lemonstar99 <dabinaseo@gmail.com>
2021-10-26 17:23:38 +0900

    Update ct_LeNet.py

--- src/networks/ct_LeNet.py
+++ src/networks/ct_LeNet.py
@@ -103,7 +103,7 @@ class CT_LeNet_Autoencoder(BaseNet):
         # x = self.pool(F.leaky_relu(self.bn2d3(x)))
         x = x.view(x.size(0), -1)
         # x = self.bn1d(self.fc1(x))
-        x = x.view(x.size(0), int(self.rep_dim / (4 * 4)), 4, 4)
+        # x = x.view(x.size(0), int(self.rep_dim / (4 * 4)), 4, 4)
         # x = x.view(200, 128, 5, 5)
         x = F.leaky_relu(x)
         x = self.deconv1(x)
