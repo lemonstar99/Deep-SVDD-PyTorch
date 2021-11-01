7c1c2029c7893849ec19a9ebcbea325e5821eeb5
lemonstar99 <dabinaseo@gmail.com>
2021-11-01 17:16:33 +0900

    Update ae_trainer.py

--- src/optim/ae_trainer.py
+++ src/optim/ae_trainer.py
@@ -121,7 +121,7 @@ class AETrainer(BaseTrainer):
         print("scores: ", scores.shape)
 
         # TODO error
-        auc = roc_auc_score(labels.reshape(1, -1), scores.reshape(1, -1))
+        auc = roc_auc_score(labels, scores.reshape(1, -1))
         logger.info('Test set AUC: {:.2f}%'.format(100. * auc))
 
         test_time = time.time() - start_time
