b4f2e5f857c923130115bf8b27f9b31fba7b7240
lemonstar99 <dabinaseo@gmail.com>
2021-11-02 13:25:36 +0900

    Update ct.py

--- src/datasets/ct.py
+++ src/datasets/ct.py
@@ -68,17 +68,16 @@ class CT_Dataset(TorchvisionDataset):
         
         target_transform = transforms.Lambda(lambda x: int(x in self.outlier_classes))
 
-        train_set = MyCT(root=self.root, idx=test_count, x_values=x, y_values=y, train=True,
+        self.train_set = MyCT(root=self.root, idx=test_count, x_values=x, y_values=y, train=True,
                             transform=transform, target_transform=target_transform)
         
         train_idx_normal = get_target_label_idx(y, self.normal_classes)
         train_set = Subset(train_set, train_idx_normal)
             
-        test_set = MyCT(root=self.root, idx=test_count, x_values=x, y_values=y, train=False,
+        self.test_set = MyCT(root=self.root, idx=test_count, x_values=x, y_values=y, train=False,
                                 transform=transform, target_transform=target_transform)
         
-        self.train_set = TensorDataset(train_set)
-        self.test_set = TensorDataset(test_set)
+        
 
         print("train set: ", self.train_set)
         print("test set: ", self.test_set)
