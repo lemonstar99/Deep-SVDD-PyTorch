ffc3b4956ac75a2ec73e49b39e2dc83b28621de7
lemonstar99 <dabinaseo@gmail.com>
2021-11-04 16:12:49 +0900

    Update sad_LeNet.py

--- src/networks/sad_LeNet.py
+++ src/networks/sad_LeNet.py
@@ -66,10 +66,10 @@ class SAD_LeNet_Autoencoder(BaseNet):
         self.deconv1 = nn.ConvTranspose2d(int(64 / (2 * 2)), 128, 5, bias=False, padding=2)
         nn.init.xavier_uniform_(self.deconv1.weight, gain=nn.init.calculate_gain('leaky_relu'))
         self.bn2d4 = nn.BatchNorm2d(128, eps=1e-04, affine=False)
-        self.deconv2 = nn.ConvTranspose2d(128, 64, 3, bias=False, padding=2)
+        self.deconv2 = nn.ConvTranspose2d(128, 64, 5, bias=False, padding=2)
         nn.init.xavier_uniform_(self.deconv2.weight, gain=nn.init.calculate_gain('leaky_relu'))
         self.bn2d5 = nn.BatchNorm2d(64, eps=1e-04, affine=False)
-        self.deconv3 = nn.ConvTranspose2d(64, 32, 3, bias=False, padding=2)
+        self.deconv3 = nn.ConvTranspose2d(64, 32, 5, bias=False, padding=2)
         nn.init.xavier_uniform_(self.deconv3.weight, gain=nn.init.calculate_gain('leaky_relu'))
         self.bn2d6 = nn.BatchNorm2d(32, eps=1e-04, affine=False)
         self.deconv4 = nn.ConvTranspose2d(32, 93, 4, bias=False, padding=2)
@@ -88,31 +88,31 @@ class SAD_LeNet_Autoencoder(BaseNet):
         x = self.pool(F.leaky_relu(self.bn2d2(x)))
         print("05: ", x.size()) # [200, 64, 4, 1]
         x = self.conv3(x)
-        print("06: ", x.size()) # [200, 128, 6, 3]
+        print("06: ", x.size()) # [200, 128, 5, 2]
         x = self.pool(F.leaky_relu(self.bn2d3(x)))
-        print("07: ", x.size()) # [200, 128, 3, 1]
+        print("07: ", x.size()) # [200, 128, 2, 1]
         x = x.view(x.size(0), -1)
-        print("08: ", x.size())
+        print("08: ", x.size()) # [200, 256]
         x = self.bn1d(self.fc1(x))
-        print("09: ", x.size())
+        print("09: ", x.size()) # [200, 64]
         x = x.view(x.size(0), int(64 / (2 * 2)), 2, 2)
-        print("10: ", x.size())
+        print("10: ", x.size()) # [200, 16, 2, 2]
         x = F.leaky_relu(x)
-        print("11: ", x.size())
+        print("11: ", x.size()) # [200, 16, 2, 2]
         x = self.deconv1(x)
-        print("12: ", x.size())
+        print("12: ", x.size()) # [200, 128, 2, 2]
         x = F.interpolate(F.leaky_relu(self.bn2d4(x)), scale_factor=2)
-        print("13: ", x.size())
+        print("13: ", x.size()) # [200, 128, 4, 4]
         x = self.deconv2(x)
-        print("14: ", x.size())
+        print("14: ", x.size()) # [200, 64, 2, 2]
         x = F.interpolate(F.leaky_relu(self.bn2d5(x)), scale_factor=2)
-        print("15: ", x.size())
+        print("15: ", x.size()) # [200, 64, 4, 4]
         x = self.deconv3(x)
-        print("16: ", x.size())
+        print("16: ", x.size()) # [200, 32, 2, 2]
         x = F.interpolate(F.leaky_relu(self.bn2d6(x)), scale_factor=2)
-        print("17: ", x.size())
+        print("17: ", x.size()) # [200, 32, 4, 4]
         x = self.deconv4(x)
-        print("18: ", x.size())
+        print("18: ", x.size()) # [200, 93, 3, 3]
         x = torch.sigmoid(x)
-        print("19: ", x.size())
+        print("19: ", x.size()) # [200, 93, 3, 3]
         return x[:,:,:,0]
