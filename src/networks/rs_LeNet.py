2b3baee2a746d5c9da6e33161db431b8aa32b480
lemonstar99 <dabinaseo@gmail.com>
2021-11-04 15:47:16 +0900

    Update rs_LeNet.py

--- src/networks/rs_LeNet.py
+++ src/networks/rs_LeNet.py
@@ -72,7 +72,7 @@ class RS_LeNet_Autoencoder(BaseNet):
         self.deconv3 = nn.ConvTranspose2d(64, 32, 3, bias=False, padding=2)
         nn.init.xavier_uniform_(self.deconv3.weight, gain=nn.init.calculate_gain('leaky_relu'))
         self.bn2d6 = nn.BatchNorm2d(32, eps=1e-04, affine=False)
-        self.deconv4 = nn.ConvTranspose2d(32, 30, 4, bias=False, padding=2)
+        self.deconv4 = nn.ConvTranspose2d(32, 30, 5, bias=False, padding=2)
         nn.init.xavier_uniform_(self.deconv4.weight, gain=nn.init.calculate_gain('leaky_relu'))
 
     def forward(self, x):
