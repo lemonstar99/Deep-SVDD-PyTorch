744088e74b41f7449a70cc89539b28621bf34099
lemonstar99 <dabinaseo@gmail.com>
2021-11-04 16:07:29 +0900

    Update sad_LeNet.py

--- src/networks/sad_LeNet.py
+++ src/networks/sad_LeNet.py
@@ -54,7 +54,7 @@ class SAD_LeNet_Autoencoder(BaseNet):
         self.conv1 = nn.Conv2d(93, 32, 3, bias=False, padding=2)
         nn.init.xavier_uniform_(self.conv1.weight, gain=nn.init.calculate_gain('leaky_relu'))
         self.bn2d1 = nn.BatchNorm2d(32, eps=1e-04, affine=False)
-        self.conv2 = nn.Conv2d(32, 64, 3, bias=False, padding=2)
+        self.conv2 = nn.Conv2d(32, 64, 4, bias=False, padding=2)
         nn.init.xavier_uniform_(self.conv2.weight, gain=nn.init.calculate_gain('leaky_relu'))
         self.bn2d2 = nn.BatchNorm2d(64, eps=1e-04, affine=False)
         self.conv3 = nn.Conv2d(64, 128, 3, bias=False, padding=2)
@@ -76,21 +76,21 @@ class SAD_LeNet_Autoencoder(BaseNet):
         nn.init.xavier_uniform_(self.deconv4.weight, gain=nn.init.calculate_gain('leaky_relu'))
 
     def forward(self, x):
-        print("00: ", x.size())
+        print("00: ", x.size()) # [200, 93, 13]
         x = x.unsqueeze(3)
-        print("01: ", x.size())
+        print("01: ", x.size()) # [200, 93, 13, 1]
         x = self.conv1(x)
-        print("02: ", x.size())
+        print("02: ", x.size()) # [200, 32, 15, 3]
         x = self.pool(F.leaky_relu(self.bn2d1(x)))
-        print("03: ", x.size())
+        print("03: ", x.size()) # [200, 32, 7, 1]
         x = self.conv2(x)
-        print("04: ", x.size())
+        print("04: ", x.size()) # [200, 64, 9, 3]
         x = self.pool(F.leaky_relu(self.bn2d2(x)))
-        print("05: ", x.size())
+        print("05: ", x.size()) # [200, 64, 4, 1]
         x = self.conv3(x)
-        print("06: ", x.size())
+        print("06: ", x.size()) # [200, 128, 6, 3]
         x = self.pool(F.leaky_relu(self.bn2d3(x)))
-        print("07: ", x.size())
+        print("07: ", x.size()) # [200, 128, 3, 1]
         x = x.view(x.size(0), -1)
         print("08: ", x.size())
         x = self.bn1d(self.fc1(x))
