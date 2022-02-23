b27ece340da61ec71383166f3ab25fe7f8659a6a
서다빈 <44111674+sodabeans@users.noreply.github.com>
2022-02-23 19:27:56 +0900

    Add files via upload

--- src/ct_script.sh
+++ src/ct_script.sh
@@ -77,3 +77,17 @@ python main.py ct ct_LeNet ../log/ct_test ../data/ct --objective one-class \
 python main.py ct ct_LeNet ../log/ct_test ../data/ct --objective one-class \
 --lr 0.0001 --n_epochs 150 --lr_milestone 50 --batch_size 200 --weight_decay 0.5e-6 \
 --pretrain True --ae_lr 0.0001 --ae_n_epochs 350 --ae_lr_milestone 50 --ae_batch_size 200 --ae_weight_decay 0.5e-3 --normal_class 19;
+
+
+
+
+
+# python main.py ct ct_LeNet ../log/ct_test ../data/ct --objective one-class \
+# --lr 0.0001 --n_epochs 10 --lr_milestone 50 --batch_size 200 --weight_decay 0.5e-6 \
+# --pretrain True --ae_lr 0.0001 --ae_n_epochs 30 --ae_lr_milestone 50 --ae_batch_size 200 --ae_weight_decay 0.5e-3 --normal_class 0;
+
+
+
+# python main.py hr hr_LeNet ../log/hr_test /workspace/datasets/health --objective one-class \
+# --lr 0.0001 --n_epochs 10 --lr_milestone 50 --batch_size 200 --weight_decay 0.5e-6 \
+# --pretrain True --ae_lr 0.0001 --ae_n_epochs 10 --ae_lr_milestone 50 --ae_batch_size 200 --ae_weight_decay 0.5e-3 --normal_class 0;
